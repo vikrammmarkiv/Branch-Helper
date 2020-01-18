2838d7f266444e36a0345558af78db9bba57f948
Vikram <vikrammmarkiv@gmail.com>
2020-01-19 03:42:39 +0530

    Changes 2

--- app/src/main/java/com/example/branchiohelper/LinkCreateActivity.java
+++ app/src/main/java/com/example/branchiohelper/LinkCreateActivity.java
@@ -1,8 +1,20 @@
 package com.example.branchiohelper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.branchiohelper.adapter.FormAdapter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class LinkCreateActivity extends AppCompatActivity {
 
@@ -10,5 +22,34 @@ public class LinkCreateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_link_create);
+        RecyclerView recyclerView = findViewById(R.id.editrecyclerview);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        ArrayList<String> formData = new ArrayList<>();
+        formData.add("standard");
+        FormAdapter formAdapter = new FormAdapter(formData);
+        recyclerView.setItemViewCacheSize(20);
+        recyclerView.setAdapter(formAdapter);
+
+        findViewById(R.id.add_data).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                formAdapter.mFormState.add("standard");
+                formAdapter.notifyItemInserted(formAdapter.mFormState.size());
+                recyclerView.smoothScrollToPosition(formAdapter.mFormState.size());
+            }
+        });
+    }
+    protected void onLeaveThisActivity() {
+        overridePendingTransition(R.anim.enter_from_left, R.anim.exit_to_right);
+    }
+    @Override
+    public void finish() {
+        super.finish();
+        onLeaveThisActivity();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
